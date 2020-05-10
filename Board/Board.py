fe57ce9663b5ed175efd2d652e985e39c2ac0833
B14nk <tgo95@gmx.de>
2020-05-10 14:58:35 +0200

    tests in RandomProblem.py

--- Board/Board.py
+++ Board/Board.py
@@ -29,3 +29,25 @@ class Board:
 
     def getValue2(self, xBoard, yBoard, xField, yField):
         return self.board[yBoard].boardRow[xBoard].field[yField].fieldRow[xField]
+    
+    def setValue(self, x: int, y: int, value: int):
+        """
+
+        :param x: The x Coordinate of the Board
+        :param y: The y Coordinate of the Board
+        :return: None
+        """
+
+        self.board[math.floor(y / self.length)].boardRow[math.floor(x / self.length)].field[y % self.length].fieldRow[
+            x % self.length] = value
+
+    def getValue(self, x: int, y: int):
+        """
+
+        :param x: The x Coordinate of the Board
+        :param y: The y Coordinate of the Board
+        :return: The value of the specified Coordinate
+        """
+        return self.board[math.floor(y / self.length)].boardRow[math.floor(x / self.length)].field[
+            y % self.length].fieldRow[x % self.length]
+
--- ProblemGenerator/RandomProblem.py
+++ ProblemGenerator/RandomProblem.py
@@ -1,4 +1,30 @@
 from ProblemGenerator.ProblemBaseClass import ProblemFinder
 
+# seed the pseudorandom number generator
+from random import seed
+from random import random
+from Board.Board import Board
+
 class RandomProblem(ProblemFinder):
     super
+
+    def __init__(self, board: Board):
+        self.board = board
+
+    def returnProblem(self, s: int):
+        """
+
+        :param s: value for the seed
+        :return:
+        """
+        # seed random number generator
+        seed(s)
+        # generate some random numbers
+        # print(random(), random(), random())
+
+
+    def returnProblemSolution(self):
+        """
+
+        :return:
+        """
