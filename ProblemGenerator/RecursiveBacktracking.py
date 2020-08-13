d9b8c7ecec20382fcfd67c3b250e5e88c0a4bc07
Tarek Stelzle <tarek.stelzle@gmx.de>
2020-08-13 21:10:16 +0200

    Recursive works until 4x4 Board

---
+++ ProblemGenerator/RecursiveBacktracking.py
@@ -0,0 +1,94 @@
+import math
+import random
+
+import Board.Board
+import ProblemGenerator.difficulties as difficulties
+from ProblemGenerator.ProblemBaseClass import ProblemFinder
+
+
+class RecursiveBacktracking(ProblemFinder):
+
+    def __init__(self, board: Board):
+        super().__init__(board)
+        self.board = board
+        self.setValues = {}
+        self.initializeSetValues()
+
+    def returnProblemSolution(self):
+       self.recursive(0, 0, 0, 1, 0)
+
+    def recursive(self, x: int, y: int, valIndex: int, tried: int, count: int):
+        if (y == self.board.getBoardLength() and x == 0):
+            return
+
+        if (y < self.board.getBoardLength() and x < self.board.getBoardLength()):
+            val = self.setValues[self.getIdentifier(x, y)][valIndex]
+            row = self.checkRow(y, val)
+            column = self.checkColumn(x, val)
+            box = self.checkBox(x, y, val)
+            if (row and column and box):
+                self.resetFollowingSetValues(x, y)
+                self.board.setValue(x, y, val)
+                self.setValues[self.getIdentifier(x, y)].remove(val)
+                if x == self.board.getBoardLength() - 1:
+                    return self.recursive(0, y+1, 0, 1, count+1)
+                else:
+                    return self.recursive(x+1, y, 0, 1, count+1)
+            elif tried >= len(self.setValues[self.getIdentifier(x, y)]):
+                self.board.setValue(x, y, -1)
+                if x == 0:
+                    return self.recursive(self.board.getBoardLength()-1, y-1, 0, 1, count+1)
+                else:
+                    return self.recursive(x-1, y, 0, 1, count+1)
+            else:
+                return self.recursive(x, y, valIndex+1, tried+1, count+1)
+
+
+    def returnProblem(self, difficulty: difficulties):
+        if self.board.getValue(0, 0) == None:
+            self.returnProblemSolution()
+        for val in range(0, math.floor(self.board.getMaxNumberOfEntries() * difficulty)):
+            x = random.randint(0, self.board.getBoardLength() - 1)
+            y = random.randint(0, self.board.getBoardLength() - 1)
+            self.board.setValue(x, y, -1)
+
+    def initializeSetValues(self):
+        """
+        Initializes the setValues dictionary, which stores which values can be used in the sudoku at this position.
+        :return: None
+        """
+        for x in range(0, self.board.getBoardLength()):
+            for y in range(0, self.board.getBoardLength()):
+                self.setValues[self.getIdentifier(x, y)] = self.randomList(self.board.getBoardLength() + 1)
+
+    def getIdentifier(self, x: int, y: int):
+        """
+        Returns the string which identifies the fields in the sudoku for the setValues dictionary.
+        :param x: x-axis of the sudoku field
+        :param y: y-axis of the sudoku field
+        :return: identifier string
+        """
+        return str(x) + "_" + str(y)
+
+    def resetFollowingSetValues(self, x: int, y: int):
+        """
+        Reset the values in the setValues dictionary after the given x,y position.
+        :param x: x-axis of the sudoku field
+        :param y: y-axis of the sudoku field
+        :return: None
+        """
+        for x_1 in range(x + 1, self.board.getBoardLength()):
+            self.setValues[self.getIdentifier(x_1, y)] = self.randomList(self.board.getBoardLength() + 1)
+        for y_1 in range(y + 1, self.board.getBoardLength()):
+            for x_1 in range(0, self.board.getBoardLength()):
+                self.setValues[self.getIdentifier(x_1, y_1)] = self.randomList(self.board.getBoardLength() + 1)
+
+    def randomList(self, length: int):
+        """
+        Builds and returns a randomized array of values from one to the given length.
+
+        :param length: the range for the array
+        :return: randomized array of values within the given length
+        """
+        sortedList = list(range(1, length))
+        return random.sample(sortedList, len(sortedList))
