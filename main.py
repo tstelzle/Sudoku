92888cf483ed22aff332521ab82ff7df4cf91e87
Tarek Stelzle <tarek.stelzle@gmx.de>
2020-08-15 21:34:18 +0200

    Linter Fixes

--- main.py
+++ main.py
@@ -8,27 +8,27 @@ from ProblemGenerator import ProblemBaseClass
 from output import PdfService
 
 
-def runSolution(sudoku: Board, algorithmClass: ProblemBaseClass):
+def run_solution(sudoku: Board, algorithm_class: ProblemBaseClass):
     """
     Runs the given algorithm and returns the sudoku solution.
     :param sudoku: the sudoku board
-    :param algorithmClass: the class of the algorithm to use
+    :param algorithm_class: the class of the algorithm to use
     :return: the sudoku board
     """
-    algorithmus = algorithmClass(sudoku)
-    pdfPrinter = PdfService.PdfPrinter(sudoku)
+    algorithm = algorithm_class(sudoku)
+    pdf_printer = PdfService.PdfPrinter(sudoku)
     print('The Sudoku Solution:')
-    starttime = time.time()
-    algorithmus.returnProblemSolution()
-    endtime = time.time()
-    print('Duration: ' + str(endtime - starttime) + "s")
+    start_time = time.time()
+    algorithm.returnProblemSolution()
+    end_time = time.time()
+    print('Duration: ' + str(end_time - start_time) + "s")
     sudoku.printBoard()
     title = 'Sudoku-Solution'
-    pdfPrinter.printSudoku(title, getFileName(sudoku, title))
+    pdf_printer.print_sudoku(title, get_file_name(sudoku, title))
     return sudoku
 
 
-def getFileName(sudoku: Board, title: str):
+def get_file_name(sudoku: Board, title: str):
     """
     Returns the filename of the pdf to make, with a timestamp
     :param sudoku: the sudoku board
@@ -36,42 +36,42 @@ def getFileName(sudoku: Board, title: str):
     :return: filename
     """
     timestamp = str(int(time.time()))
-    boardsize = str(sudoku.length)
-    return title + "_" + timestamp + "_" + boardsize
+    board_size = str(sudoku.length)
+    return title + "_" + timestamp + "_" + board_size
 
 
-def runProblem(sudoku: Board, algorithmClass: ProblemBaseClass, difficulty: difficulties):
+def run_problem(sudoku: Board, algorithm_class: ProblemBaseClass, difficulty: difficulties):
     """
     Runs the given algorithm and returns the sudoku problem.
     :param sudoku: the sudoku board
-    :param algorithmClass: the class of the algorithm to use
+    :param algorithm_class: the class of the algorithm to use
     :param difficulty: the difficulty of the problem specified by the enum
     :return: the sudoku board
     """
-    algorithmus = algorithmClass(sudoku)
-    pdfPrinter = PdfService.PdfPrinter(sudoku)
+    algorithm = algorithm_class(sudoku)
+    pdf_printer = PdfService.PdfPrinter(sudoku)
     print('The Sudoku Problem:')
-    algorithmus.returnProblem(difficulty)
+    algorithm.returnProblem(difficulty)
     sudoku.printBoard()
     title = 'Sudoku-Problem'
-    pdfPrinter.printSudoku(title, getFileName(sudoku, title))
+    pdf_printer.print_sudoku(title, get_file_name(sudoku, title))
     return sudoku
 
 
-def runSolutionAndProblem(sudoku: Board, algorithmClass: ProblemBaseClass, difficulty=difficulties.HARD):
+def run_solution_and_problem(sudoku: Board, algorithm_class: ProblemBaseClass, difficulty=difficulties.HARD):
     """
     Runs the two methods 'runSolution' and 'runProblem' to calculate the sudoku solution and problem.
     :param sudoku: the sudoku board
-    :param algorithmClass: the class of the algorithm to use
+    :param algorithm_class: the class of the algorithm to use
     :param difficulty: the difficulty of the problem specified by the enum
     :return:
     """
-    sudoku_sol = runSolution(sudoku, algorithmClass)
-    sudoku_prob = runProblem(sudoku_sol, algorithmClass, difficulty)
+    sudoku_sol = run_solution(sudoku, algorithm_class)
+    sudoku_prob = run_problem(sudoku_sol, algorithm_class, difficulty)
     return [sudoku_sol, sudoku_prob]
 
 
-def printBoardInformation(sudoku: Board):
+def print_board_information(sudoku: Board):
     """
     Prints the information about the board size.
     :param sudoku: the sudoku board
@@ -84,7 +84,7 @@ def printBoardInformation(sudoku: Board):
     print("")
 
 
-def readBoardParameter():
+def read_board_parameter():
     """
     Reads the input parameters. And returns the size of the board.
     :return: int
@@ -95,10 +95,10 @@ def readBoardParameter():
 
 
 def main():
-    boardSize = readBoardParameter() if readBoardParameter() > 0 else 3
-    sudoku = Board(boardSize)
-    printBoardInformation(sudoku)
-    results = runSolutionAndProblem(sudoku, BruteForceBacktracking.BruteForceBacktracking)
+    board_size = read_board_parameter() if read_board_parameter() > 0 else 3
+    sudoku = Board(board_size)
+    print_board_information(sudoku)
+    run_solution_and_problem(sudoku, BruteForceBacktracking.BruteForceBacktracking)
 
 
 if __name__ == "__main__":
